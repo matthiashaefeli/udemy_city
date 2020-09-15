2467b46704dde338f5320ed93f2e7c6e55379f36
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-15 15:07:05 -0600

    Tour and TourList components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './App.scss';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Navbar from './components/Navbar/Navbar';
+import TourList from './components/TourList';
 
 class App extends Component {
   render() {
     return (
-      <>
-        <Navbar></Navbar>
-      </>
+      <React.Fragment>
+        <Navbar />
+        <TourList />
+      </React.Fragment>
     )
   }
 }
---
+++ src/components/TourList/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Tour from '../Tour';
+
+export default class TourList extends Component {
+  render() {
+    return (
+      <div>
+        TourList
+        <Tour />
+      </div>
+    )
+  }
+}
