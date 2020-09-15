2ec80ec12ff9ad15c049b0308ff494a5e669a8c0
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-14 19:02:11 -0600

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Navbar from './components/Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <h1>app</h1>
+      <>
+        <Navbar></Navbar>
+      </>
     )
   }
 }
