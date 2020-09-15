05b7fc7b37efab465efc2396271c22ab1d361c08
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-15 15:27:49 -0600

    tour list style and lsit

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import Tour from '../Tour';
+import './tourlist.scss';
+import { tourData } from '../../tourData';
 
 export default class TourList extends Component {
+  state = {
+    tours: tourData
+  };
+
+  removeTour = id => {
+    console.log(id)
+  }
+
   render() {
+    const { tours } = this.state;
+
     return (
-      <div>
-        TourList
-        <Tour />
-      </div>
+      <section className='tourlist'>
+        {
+          tours.map(tour => (<Tour key={tour.id} tour={tour} removeTour={this.removeTour}></Tour>))
+        }
+      </section>
     )
   }
 }
